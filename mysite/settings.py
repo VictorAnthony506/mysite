d81624b0ca6233c08050b405ff7b80b58541e7e2
VictorAnthony506 <victor0anthony@gmail.com>
2022-10-29 14:31:55 +0100

    set

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,6 +134,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
